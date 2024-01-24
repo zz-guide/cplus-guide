07d0a3324255cf1c38d679469c3e830bd0858b50
xulei131401 <373045134@qq.com>
2024-01-24 16:30:06 +0800

    fix

---
+++ foundation/t_class/first_identify/main.cpp
@@ -0,0 +1,15 @@
+#include "main.h"
+using namespace std;
+
+class Student {
+public:
+    string name = "哈哈";
+};
+int main()
+{
+    Student stu;
+    cout << stu.name << endl;
+    int a = 123;
+    cout << a << endl;
+    return 0;
+}
